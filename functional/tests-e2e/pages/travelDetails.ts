012134b1615cdcdad1626aafaa5cd2939abd6352
Keerthivasan Rc <keerthivasan.rc@SPI.COM>
2019-05-16 18:49:43 +0530

    Scripts

---
+++ functional/tests-e2e/pages/travelDetails.ts
@@ -0,0 +1,7 @@
+import {browser, by, element, ElementArrayFinder, ElementFinder, ExpectedConditions} from 'protractor';
+
+export class travelDetails {
+
+    private adultTitleWE: ElementFinder;
+    private adultFnameWE: ElementFinder;
+}

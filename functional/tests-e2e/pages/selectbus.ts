5a97d82639bab7ea34111bbd5412d12114c59c2d
Keerthivasan Rc <keerthivasan.rc@SPI.COM>
2019-05-27 14:29:59 +0530

    mobikwik working

--- functional/tests-e2e/pages/selectbus.ts
+++ functional/tests-e2e/pages/selectbus.ts
@@ -7,16 +7,20 @@ export class selectBus {
     private toWE : ElementFinder;
     private dateWE : ElementFinder;
     private calenderWE : ElementFinder;
+    private from1st: ElementFinder
     private searchWE : ElementFinder;
+    private showWE: ElementFinder;
     private EC = ExpectedConditions;
 
     constructor() {
 
         this.fromWE=element(by.css("form>div:nth-child(1)>div>ng-select>div:nth-child(1)>div>div>input"));
+        this.from1st = element(by.css("div.ng-option:nth-child(1)"));
         this.toWE = element(by.css("form>div:nth-child(2)>div>ng-select>div:nth-child(1)>div>div>input"));
         this.dateWE = element(by.css("table.days.weeks tbody:nth-child(2) tr:nth-child(5) td:nth-child(6)"));
         this.calenderWE = element(by.css(".spright8:nth-child(3)>div>input"));
         this.searchWE = element(by.buttonText("Search"));
+        this.showWE = element(by.css("div.row:nth-child(1) > div.col-md-10"));
         
     }
 
@@ -26,11 +30,16 @@ export class selectBus {
         await browser.wait(this.EC.visibilityOf(this.fromWE),10000);
         await this.fromWE.click();
         await this.fromWE.sendKeys(from)
+        await browser.wait(this.EC.visibilityOf(this.from1st),10000);
+        await this.from1st.click();
         await browser.wait(this.EC.visibilityOf(this.toWE),10000);
         await this.toWE.click();
         await this.toWE.sendKeys(to);
+        await this.from1st.click();
         await this.calenderWE.click();
         await this.dateWE.click();
         await this.searchWE.click();
+        await browser.wait(this.EC.visibilityOf(this.showWE),5000);
+        expect(await this.showWE.getText()).toContain("Show")
     }
 }

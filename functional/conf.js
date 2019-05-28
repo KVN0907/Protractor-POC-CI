5573a97c8b4f11de02397c3a8b29d38c79bf5d8f
Keerthivasan Rc <keerthivasan.rc@SPI.COM>
2019-05-28 16:22:21 +0530

    Protractoe E2E - With Reporter

--- functional/conf.js
+++ functional/conf.js
@@ -1,18 +1,24 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const {SpecReporter} = require('jasmine-spec-reporter');
+
+var HtmlReporter = require('protractor-beautiful-reporter');
 
 exports.config = {
     allScriptsTimeout: 60000,
     specs: [
         './tests-e2e/specs/*.ts',
     ],
-  
-    capabilities: {
-        'browserName': 'chrome',
-       /*  chromeOptions: {
-             prefs: {
+    multiCapabilities: [{
+        'browserName': 'chrome'
+      }, {
+        'browserName': 'firefox'
+      }],
+   /*  mutliplecapabilities: {
+        'browserName': 'firefox',
+        'browserName': 'chrome'
+        chromeOptions: {
+            prefs: {
                 download: {
                     'prompt_for_download': false,
                     'directory_upgrade': true,
@@ -22,20 +28,22 @@ exports.config = {
                     }
 
                 }
-            }, */
-          /*   args: [
+            },
+            args: [
                 "--headless",
                 "--disable-gpu",
                 "--window-size=1920,1080",
                 "--no-sandbox",
                 "--disable-dev-shm-usage"
             ],
-              */
+            
 
-       // }
-    },
+        } 
+    }, */
     directConnect: true,
-    framework: 'jasmine',
+    baseUrl: '',
+    awsUrl: '',
+    framework: 'jasmine2',
     jasmineNodeOpts: {
         showColors: true,
         defaultTimeoutInterval: 360000,
@@ -48,54 +56,21 @@ exports.config = {
     },
     onPrepare() {
         browser.ignoreSynchronization = false;
-        require('jasmine2-protractor-utils');
+		browser.params.TIMEOUT_INTERVAL = 360000;
         require('ts-node').register({
             project: 'tests-e2e/tsconfig.e2e.json'
         });
-        let env = jasmine.getEnv();
-        browser.params.TIMEOUT_INTERVAL = 360000; // global interval timeout used in spec files
-        env.addReporter(new SpecReporter({
-            spec: {
-                displayStacktrace: true,
-                displayFailuresSummary: true,
-                displaySuccessesSummary: true,
-                displayPendingSummary: true,
-                displaySuccessfulSpec: true,
-                displayFailedSpec: true,
-                displayPendingSpec: true,
-                displaySuiteNumber: true,
-                displaySpecDuration: true
-            }
-        }));
-        global.browser.getProcessedConfig().then(function (config) {
-        });
 
-        let specs = [];
-        let orgSpecFilter = jasmine.getEnv().specFilter;
-        jasmine.getEnv().specFilter = function (spec) {
-            specs.push(spec);
-            return orgSpecFilter(spec);
-        };
-        jasmine.getEnv().addReporter(new function () {
-            this.specDone = function (result) {
-                if (result.failedExpectations.length > 0) {
-                    specs.forEach(function (spec) {
-                        spec.disable()
-                    });
-                }
-            };
-        });
-
-
+        jasmine.getEnv().addReporter(new HtmlReporter({
+            preserveDirectory: false,
+            takeScreenShotsOnlyForFailedSpecs: true,
+            screenshotsSubfolder: 'Screenshot',
+            jsonsSubfolder: 'Json',
+            baseDirectory: './reports',
+            gatherBrowserLogs: false,
+            docTitle: 'Protractor_POC',
+                      
+        }).getJasmine2Reporter());
     },
-    plugins: [{
-        package: 'jasmine2-protractor-utils',
-        disableHTMLReport: false,
-        disableScreenshot: false,
-        screenshotPath: './reports/screenshots',
-        screenshotOnExpectFailure: true,
-        screenshotOnSpecFailure: true,
-        clearFoldersBeforeTest: true,
-        htmlReportDir: './reports/htmlReports'
-    }]
+
 };
